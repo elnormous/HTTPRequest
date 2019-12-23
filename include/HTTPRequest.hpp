87acf1f73ef7eb9396bc85c631f27a462380ffcf
Elviss Strazdins <elviss@elviss.lv>
2019-12-23 02:47:07 +0200

    Move WinSock class and utility functions to inline namespace

--- include/HTTPRequest.hpp
+++ include/HTTPRequest.hpp
@@ -54,62 +54,6 @@ namespace http
         explicit ResponseError(const std::string& str): std::runtime_error(str) {}
     };
 
-#ifdef _WIN32
-    class WinSock final
-    {
-    public:
-        WinSock()
-        {
-            WSADATA wsaData;
-            const int error = WSAStartup(MAKEWORD(2, 2), &wsaData);
-            if (error != 0)
-                throw std::system_error(error, std::system_category(), "WSAStartup failed");
-
-            if (LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) != 2)
-            {
-                WSACleanup();
-                throw std::runtime_error("Invalid WinSock version");
-            }
-
-            started = true;
-        }
-
-        ~WinSock()
-        {
-            if (started) WSACleanup();
-        }
-
-        WinSock(const WinSock&) = delete;
-        WinSock& operator=(const WinSock&) = delete;
-
-        WinSock(WinSock&& other) noexcept:
-            started(other.started)
-        {
-            other.started = false;
-        }
-
-        WinSock& operator=(WinSock&& other) noexcept
-        {
-            if (&other == this) return *this;
-            if (started) WSACleanup();
-            started = other.started;
-            other.started = false;
-            return *this;
-        }
-
-    private:
-        bool started = false;
-    };
-#endif
-
-    inline int getLastError() noexcept
-    {
-#ifdef _WIN32
-        return WSAGetLastError();
-#else
-        return errno;
-#endif
-    }
 
     enum class InternetProtocol: uint8_t
     {
@@ -117,11 +61,71 @@ namespace http
         V6
     };
 
-    constexpr int getAddressFamily(InternetProtocol internetProtocol)
+    inline namespace detail
     {
-        return (internetProtocol == InternetProtocol::V4) ? AF_INET :
-            (internetProtocol == InternetProtocol::V6) ? AF_INET6 :
-            throw RequestError("Unsupported protocol");
+#ifdef _WIN32
+        class WinSock final
+        {
+        public:
+            WinSock()
+            {
+                WSADATA wsaData;
+                const int error = WSAStartup(MAKEWORD(2, 2), &wsaData);
+                if (error != 0)
+                    throw std::system_error(error, std::system_category(), "WSAStartup failed");
+
+                if (LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) != 2)
+                {
+                    WSACleanup();
+                    throw std::runtime_error("Invalid WinSock version");
+                }
+
+                started = true;
+            }
+
+            ~WinSock()
+            {
+                if (started) WSACleanup();
+            }
+
+            WinSock(const WinSock&) = delete;
+            WinSock& operator=(const WinSock&) = delete;
+
+            WinSock(WinSock&& other) noexcept:
+                started(other.started)
+            {
+                other.started = false;
+            }
+
+            WinSock& operator=(WinSock&& other) noexcept
+            {
+                if (&other == this) return *this;
+                if (started) WSACleanup();
+                started = other.started;
+                other.started = false;
+                return *this;
+            }
+
+        private:
+            bool started = false;
+        };
+#endif
+
+        inline int getLastError() noexcept
+        {
+#ifdef _WIN32
+            return WSAGetLastError();
+#else
+            return errno;
+#endif
+        }
+
+        constexpr int getAddressFamily(InternetProtocol internetProtocol)
+        {
+            return (internetProtocol == InternetProtocol::V4) ? AF_INET :
+                (internetProtocol == InternetProtocol::V6) ? AF_INET6 :
+                throw RequestError("Unsupported protocol");
+        }
     }
 
     class Socket final
